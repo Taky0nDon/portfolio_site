1e838c774be003b1f2bacda0376ec10b4dda2d35
Taky0ndon <char@za.rd>
2024-02-20 13:35:18 -0500

    Added html files for subsections, created class for secondary header

--- app.py
+++ app.py
@@ -7,11 +7,24 @@ app.config['SECRET_KEY'] = None
 
 @app.route('/')
 def home():
-    return render_template("header.html")
-
-@app.route('/test')
-def test():
     return render_template("index.html")
 
+@app.route('/projects')
+def show_projects_page():
+    return render_template("projects.html")
+
+@app.route('/about-me')
+def show_about_me_page():
+    return render_template("about-me.html")
+
+@app.route('/about-site')
+def show_about_site_page():
+    return render_template("about-site.html")
+
+@app.route('/contact')
+def show_contact_page():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
