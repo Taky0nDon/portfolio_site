f637626576e3a3d7573d416e94e1ecd1e999a51d
Taky0ndon <char@za.rd>
2024-02-28 19:27:20 -0500

    allowed second image URL to be empty

--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ from flask_ckeditor import CKEditorField
 class AddProjectForm(FlaskForm):
     project_title = StringField("Project Title", validators=[DataRequired()])
     project_img_url_1 = StringField("Image URL 1", validators=[DataRequired()])
-    project_img_url_2 = StringField("Image URL 2", validators=[DataRequired()])
-    project_description = StringField("Project Description", validators=[DataRequired(), URL()])
+    project_img_url_2 = StringField("Image URL 2")
+    project_description = StringField("Project Description", validators=[DataRequired()])
     project_github_url = StringField("project_github_url", validators=[DataRequired()])
     add_project = SubmitField("Add Project")
 
