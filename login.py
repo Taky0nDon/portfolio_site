c62ddf1a00e8cfa14ed399373c3d4cc061ad0046
Taky0ndon <char@za.rd>
2024-03-06 19:37:39 -0500

    imported workzeug stuff

--- login.py
+++ login.py
@@ -1,10 +1,14 @@
 # Should I make a new class that inherits from LoginManager?
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class auth(LoginManager):
     def __init__(self, app):
         self.manager = LoginManager()
         self.manager.init_app(app)
+
+
 def admin_only(function):
     @wraps(function)
     def decorated_view(*args, **kwargs):
