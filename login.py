5798a10ed0a2b828750b94ae4e3bf19b9f4455d5
Taky0ndon <char@za.rd>
2024-03-08 18:22:27 -0500

    removed a lot of the logic from this file and put it in app.py(admin_only) and login (Auth)

--- login.py
+++ login.py
@@ -3,22 +3,4 @@ from flask_login import UserMixin, login_user, LoginManager, current_user, logou
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class auth(LoginManager):
-    def __init__(self, app):
-        self.manager = LoginManager()
-        self.manager.init_app(app)
-
-
-def admin_only(function):
-    @wraps(function)
-    def decorated_view(*args, **kwargs):
-        if current_user.is_anonymous or current_user.id != 1:
-            return abort(code=403)
-        else:
-            return function(*args, **kwargs)
-    return decorated_view
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
 
